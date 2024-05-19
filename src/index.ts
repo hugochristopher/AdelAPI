362f45f6bcf7a26968ea7f1510331a523c777b79
Joao-Ortega <joao_ortega17@hotmail.com>
2024-05-19 00:05:14 -0300

    Creating login response and connecting with frontend

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes/index.ts';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+}
+
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use('/', router)
 
